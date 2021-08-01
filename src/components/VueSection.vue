bed83093ce24e18aa5b4c953b9f9e2d19a227b75
Zakary <zakary.m@thewalkingnerds.com>
2021-08-01 17:49:11 +0200

    add section page, add vueSection component

---
+++ src/components/VueSection.vue
@@ -0,0 +1,85 @@
+<template>
+  <section :class="computedClass">
+    <section class="wrapper-title">
+      <h2>{{ title }}</h2>
+      <span>{{ description }}</span>
+      <b-button @click="action" type="is-primary" class="action-button"
+        >Action</b-button
+      >
+    </section>
+
+    <section>
+      <img class="imgO" :src="require(`../assets/${filename}`)" alt="" />
+    </section>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    action: {
+      type: Function,
+      default: () => {},
+    },
+    title: {
+      type: String,
+      default: null,
+    },
+    description: {
+      type: String,
+      default: null,
+    },
+    filename: {
+      type: String,
+      default: null,
+    },
+    imgPosition: {
+      type: String,
+      default: null,
+    },
+  },
+  methods: {},
+  computed: {
+    computedClass() {
+      let classname;
+      if (this.imgPosition === "left") {
+        classname = "wrapper-img-left";
+      } else {
+        classname = "wrapper-img-right";
+      }
+      return classname;
+    },
+  },
+};
+</script>
+
+<style>
+.wrapper-img-left {
+  display: flex;
+  justify-content: space-between;
+  flex-direction: row-reverse;
+  margin: 50px 20px;
+}
+.wrapper-img-right {
+  display: flex;
+  justify-content: space-between;
+  margin: 50px 20px;
+}
+.wrapper-title {
+  display: flex;
+  flex-direction: column;
+  width: 50%;
+}
+
+.action-button {
+  margin-top: 30px;
+  width: 80px;
+}
+.isLeft {
+  width: 500px;
+  background-color: red;
+}
+.imgO {
+  height: 200px;
+}
+</style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,11 +1,46 @@
 <template>
   <Layout>
     <h1>Bienvenue honorables visiteurs</h1>
+    <VueSection
+      title="Hello World"
+      description="Lorem Ipsum is simply dummy
+    text of the printing and typesetting industry. Lorem Ipsum has been the
+    industry's standard dummy text ever since the 1500s, when an unknown printer
+    took a galley of type and scrambled it to make a type specimen book. It has
+    survived not only five centuries, but also the leap into electronic
+    typesetting, remaining essentially unchanged. It was popularised in the
+    1960s with the release of Letraset sheets containing Lorem Ipsum passages,
+    and more recently with desktop publishing software like Aldus PageMaker
+    including versions of Lorem Ipsum "
+      filename="Yoda.jpg"
+      :action="good"
+      imgPosition="left"
+    />
+    <VueSection
+      title="Hello Joni"
+      description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum"
+      filename="Yoda.jpg"
+      imgPosition="right"
+      :action="notGood"
+    />
   </Layout>
 </template>
 
 <script>
-export default {};
+import VueSection from "@/components/VueSection.vue";
+export default {
+  components: {
+    VueSection,
+  },
+  methods: {
+    good() {
+      alert("Hello ! you're the best ! ");
+    },
+    notGood() {
+      alert("Sorry ! you're not the best ! ");
+    },
+  },
+};
 </script>
 
 <style>
@@ -13,6 +48,33 @@ h1 {
   font-size: 50px;
   font-weight: bold;
   text-align: center;
-  margin-top: 150px;
+}
+.wrapper-img-left {
+  display: flex;
+  justify-content: space-between;
+  flex-direction: row-reverse;
+  margin: 50px 20px;
+}
+.wrapper-img-right {
+  display: flex;
+  justify-content: space-between;
+  margin: 50px 20px;
+}
+.wrapper-title {
+  display: flex;
+  flex-direction: column;
+  width: 50%;
+}
+
+.action-button {
+  margin-top: 30px;
+  width: 80px;
+}
+.isLeft {
+  width: 500px;
+  background-color: red;
+}
+.imgO {
+  height: 200px;
 }
 </style>
